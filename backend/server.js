6386147c713f9ed0a5e45a0a96e8f32ac82fd3b7
Tyler Perata <tylerperata@gmail.com>
2022-11-05 20:14:23 -0700

    Add body parsing and error handler

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
 const port = process.env.PORT || 3000;
+
+// Custom error handler
+const {errorHandler} = require("./middleware/errorHandler");
+
+// Use .env file for environement variables
+require("dotenv").config(); 
 
 // Connect to MongoDB via Mongoose
 (require("./config/db"))();
 
 const app = express();
 
+// Body middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Router
 app.use("/", require("./routes/index.js"));
+
+// Use error handler
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Hunt Manager running on port ${port}`));
