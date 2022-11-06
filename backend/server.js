7e25c40b7f4a9c1951bc54f33ce8e9ccf4a910f6
Tyler Perata <tylerperata@gmail.com>
2022-11-05 18:07:10 -0700

    Use router

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use("/", require("./routes/index.js"));
+
+app.listen(port, () => console.log(`Hunt Manager running on port ${port}`));
