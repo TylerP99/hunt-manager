c495befb6cd1e177d9cefadb4a8cd6a766cbb61c
Tyler Perata <tylerperata@gmail.com>
2022-11-05 20:15:36 -0700

    Company routes

--- backend/routes/apis/company.js
+++ backend/routes/apis/company.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+const CompanyController = require("../../controllers/company-controller");
 
-router.get("/");
-router.post("/");
-router.put("/:id");
-router.delete("/:id");
+router.get("/", CompanyController.get_company_handler);
+
+router.post("/", CompanyController.create_company_handler);
+
+router.put("/:id", CompanyController.update_company_handler);
+
+router.delete("/:id", CompanyController.delete_company_handler);
 
 
 module.exports = router;
