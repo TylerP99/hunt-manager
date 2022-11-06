051384a4d4cdb701d8b14e645ec148ed604062f2
Tyler Perata <tylerperata@gmail.com>
2022-11-06 14:00:56 -0800

    Start connection model

--- backend/models/Connection.js
+++ backend/models/Connection.js
@@ -1,5 +1,32 @@
 const mongoose = require("mongoose");
 
-const connectionSchema = new mongoose.Schema({
-     
+const ConnectionSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    about: {
+        type: String,
+    },
+    role: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    linkedin: {
+        type: String,
+    },
+    twitter: {
+        type: String,
+    },
+    firstMessage: {
+        type: Boolean,
+        default: false,
+    },
+
 }, { timestamps: true });
+
+const Connection = mongoose.model("Connection", ConnectionSchema);
+
+module.exports = Connection;
