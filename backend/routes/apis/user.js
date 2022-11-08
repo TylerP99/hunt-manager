e53e8db8cba5d983eeb60c2acd92e2bf86d14443
Tyler Perata <tylerperata@gmail.com>
2022-11-07 22:28:01 -0800

    Add route protection

--- backend/routes/apis/user.js
+++ backend/routes/apis/user.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const UserController = require("../../controllers/user-controller");
 
+const { protectRoute } = require("../../middleware/auth");
+
 router.post("/create", UserController.createUser);
 
 router.post("/authenticate", UserController.authenticateUser);
 
-router.get("/getUser", UserController.getUser);
+router.get("/getUser", protectRoute, UserController.getUser);
 
 module.exports = router;
