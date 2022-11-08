04652cd01cf0c26f868be6822362694f138b38ec
Tyler Perata <tylerperata@gmail.com>
2022-11-07 22:28:34 -0800

    Add controllers and route protection

--- backend/routes/apis/connection.js
+++ backend/routes/apis/connection.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+const ConnectionController = require("../../controllers/connection-controller");
+const { protectRoute } = require("../../middleware/auth");
 
-router.get("/");
-router.post("/");
-router.put("/:id");
-router.delete("/:id");
+router.get("/", protectRoute, ConnectionController.get_connections_handler);
+router.post("/", protectRoute, ConnectionController.create_connection_handler);
+router.put("/:id", protectRoute, ConnectionController.update_connection_handler);
+router.delete("/:id", protectRoute, ConnectionController.delete_connection_handler);
 
 
 module.exports = router;
