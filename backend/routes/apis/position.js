f5b0ceeaa249bcbbdd38583cefcf4ca3afb90f05
Tyler Perata <tylerperata@gmail.com>
2022-11-07 22:28:42 -0800

    Add controllers and route protection

--- backend/routes/apis/position.js
+++ backend/routes/apis/position.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+const PositionController = require("../../controllers/position-controller");
+const { protectRoute } = require("../../middleware/auth");
 
-router.get("/");
-router.post("/");
-router.put("/:id");
-router.delete("/:id");
+router.get("/", protectRoute, PositionController.get_position_handler);
+router.post("/", protectRoute, PositionController.create_position_handler);
+router.put("/:id", protectRoute, PositionController.update_position_handler);
+router.delete("/:id", protectRoute, PositionController.delete_position_handler);
 
 
 module.exports = router;
