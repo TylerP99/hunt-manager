baea3dd160c0972fae42c0e10c68cb0658f2669b
Tyler Perata <tylerperata@gmail.com>
2022-11-07 22:28:12 -0800

    Add route protection

--- backend/routes/apis/company.js
+++ backend/routes/apis/company.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const CompanyController = require("../../controllers/company-controller");
+const { protectRoute } = require("../../middleware/auth");
 
-router.get("/", CompanyController.get_companies_handler);
+router.get("/", protectRoute, CompanyController.get_companies_handler);
 
-router.post("/", CompanyController.create_company_handler);
+router.post("/", protectRoute, CompanyController.create_company_handler);
 
-router.put("/:id", CompanyController.update_company_handler);
+router.put("/:id", protectRoute, CompanyController.update_company_handler);
 
-router.delete("/:id", CompanyController.delete_company_handler);
+router.delete("/:id", protectRoute, CompanyController.delete_company_handler);
 
 
 module.exports = router;
