c5329b16beb77c7ba6315e3d8ce0aed4c797156e
Tyler Perata <tylerperata@gmail.com>
2022-11-22 21:27:32 -0800

    Redux registration

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
 
 // Get user from localStorage
 const user = JSON.parse(localStorage.getItem("user"));
@@ -10,6 +11,18 @@ const initialState = {
     isLoading: false,
     message: "",
 };
+
+// Register user
+export const register = createAsyncThunk("auth/register", 
+async (user, thunkAPI) => {
+    try {
+        return await authService.register(user);
+    }
+    catch(e) {
+        const message = (e.response && e.response.data && e.response.data.message) || e.message || e.toString();
+        return thunkAPI.rejectWithValue(message);
+    }
+});
 
 export const authSlice = createSlice({
     name: "auth",
@@ -23,7 +36,23 @@ export const authSlice = createSlice({
         },
     },
     // Thunk
-    extraReducers: () => {},
+    extraReducers: (builder) => {
+        builder
+        .addCase(register.pending, (state) => {
+            state.isLoading = true;
+        })
+        .addCase(register.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isSuccess = true;
+            state.user = action.payload;
+        })
+        .addCase(register.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.message = action.payload;
+            state.user = null;
+        })
+    },
 });
 
 export const {reset} = authSlice.actions;
