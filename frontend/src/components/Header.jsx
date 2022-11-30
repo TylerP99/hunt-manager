7782e1bd65c93e4ec4944855162025aca52d5bff
Tyler Perata <tylerperata@gmail.com>
2022-11-29 21:53:03 -0800

    Change

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,27 +2,40 @@ import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa"
 
 import {Link} from "react-router-dom";
 
-const linkStyles = "flex items-center space-x-2 p-1 hover:text-gray-500";
+const linkStyles = "flex items-center space-x-2 p-1 hover:text-lightBlue";
 
 function Header() {
+  const signedIn = false;
   return (
-    <header className="flex justify-around text-2xl">
-      <div>
-        <Link className={linkStyles} to="/">Hunt Manager</Link>
-      </div>
-      <ul className="flex justify-evenly space-x-10">
-        <li>
-          <Link className={linkStyles} to="/login">
-            <FaSignInAlt className="align-baseline" /> <p>Login</p>
-          </Link>
-        </li>
-        <li>
-          <Link className={linkStyles} to="/register">
-            <FaUser />
-            <p>Register</p>
-          </Link>
-        </li>
-      </ul>
+    <header className="text-lg border-b-4 max-w-full py-2">
+      <nav className="flex justify-between max-w-7xl mx-auto">
+        <div>
+          <Link className={linkStyles} to="/">Hunt Manager</Link>
+        </div>
+        <ul className="flex justify-evenly space-x-10">
+          {signedIn ? 
+          (<li>
+            <Link className={linkStyles} to="/logout">
+              <FaSignOutAlt className="align-baseline" /> <p>Log out</p>
+            </Link>
+          </li>) 
+          : 
+          (<>
+          <li>
+            <Link className={linkStyles} to="/login">
+              <FaSignInAlt className="align-baseline" /> <p>Login</p>
+            </Link>
+          </li>
+          <li>
+            <Link className={linkStyles} to="/register">
+              <FaUser />
+              <p>Register</p>
+            </Link>
+          </li>
+          </>)
+          }
+        </ul>
+        </nav>
     </header>
   )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        "darkBlue": "#11224D",
+        "blue": "#193A6F",
+        "lightBlue": "#2C599D",
+        "lighterBlue": "#5B84C4",
+        "orange": "#F98125",
+        "lightOrange": "#FB9B50",
+      },
+    },
   },
   plugins: [],
 }
