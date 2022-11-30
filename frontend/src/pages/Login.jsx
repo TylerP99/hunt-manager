74af583cb6d9cc9068642a3011e954e4c9c43db8
Tyler Perata <tylerperata@gmail.com>
2022-11-29 22:15:50 -0800

    Move login form to login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,58 @@
-import LogInForm from "../components/LogInForm"
+import {useState} from "react";
 
 function Login() {
+
+  // Form state
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  })
+
+  // Form state destructured for better readablilty and less repeating
+  const {email, password} = formData;
+
+  // Change function for managing form state as controlled elements
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+        ...prevState,
+        [e.target.name]: e.target.value,
+    }));
+  }
+
+  // Submit function for form
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submit");
+  }
+
   return (
     <>
     <div>Login</div>
-    <LogInForm />
+    <form onSubmit={onSubmit}>
+      <section>
+        <label htmlFor="email">Email:</label>
+          <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={onChange}
+          />
+      </section>
+      <section>
+        <label htmlFor="password">Password:</label>
+        <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={onChange}
+        />
+      </section>
+      <button type="submit">Log In</button>
+    </form>
     </>
   )
 }
