ec580bd29a96998542ad1025fe3d1a35518bb87f
Tyler Perata <tylerperata@gmail.com>
2022-11-29 22:07:28 -0800

    Company page outlined

---
+++ frontend/src/pages/Company.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+function Company({companyID}) {
+  return (
+    <section>
+        <h1>Company Name</h1>
+        <ul>
+            <li><a>Position Links</a></li>
+            <li><a>Position Links</a></li>
+        </ul>
+        <ul>
+            <li>Initial contact</li>
+            <li>Follouw up</li>
+            <li>Interview</li>
+        </ul>
+        <ul>
+            <li>Melissa A. (Hiring Manager)</li>
+        </ul>
+        <p>Descriptive info</p>
+    </section>
+  )
+}
+
+export default Company
