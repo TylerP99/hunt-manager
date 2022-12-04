c39b2e719e55e554625e317760cbd8e53d0d9e5b
Tyler Perata <tylerperata@gmail.com>
2022-12-04 00:56:29 -0800

    Changes

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,92 +1,142 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const AsyncHandler = require("express-async-handler");
 
 const User = require("../models/User")
 
 const UserController = {
 
     /*
-    @desc:    
-    @route:   
-    @access:  
+    @desc:    Creates a new user in database, responding with errors or the user and jwt
+    @route:   POST /api/user/create
+    @access:  Public
     */
-    createUser: async (req, res, next) => {
-        try{
-            const user = {
-                username: req.body.username,
-                email: req.body.email,
-                password: req.body.password,
-            };
+    createUser: AsyncHandler( async (req, res) => {
+        // Get user info from request body
+        const user = {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        };
 
-            if(!user.username || !user.email || !user.password) {
-                return res.status(400).json({"msg": "All fields required."});
-            }
+        // If a field is missing, respond
+        if(!user.username || !user.email || !user.password) {
+            return res.status(400).json({"msg": "All fields required."});
+        }
 
-            if(user.password !== req.body.password2) {
-                return res.status(400).json({"msg": "Passwords must match."});
-            }
+        // If password and confirmation don't match, respond
+        if(user.password !== req.body.password2) {
+            return res.status(400).json({"msg": "Passwords must match."});
+        }
 
-            const existing = await User.findOne({email: user.email});
+        // Check if a user with provided email exists, if one does, respond
+        const existing = await User.findOne({email: user.email});
+        if(existing) return res.status(400).json({msg: "A user with that email already exists"});
 
-            if(existing) return res.status(400).json({msg: "A user with that email already exists"});
+        // Hash user password
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(user.password, saltRounds);
+        user.password = hashedPassword;
 
-            const saltRounds = 10;
-            const hashedPassword = await bcrypt.hash(user.password, saltRounds);
-            user.password = hashedPassword;
+        // Store user in db
+        const newUser = await User.create(user);
 
-            const newUser = await User.create(user);
+        // If a user isnt created, cry
+        if(!newUser) return res.status(400).json({msg: "Uh oh?"});
 
-            if(!newUser) return res.status(400).json({msg: "Uh oh?"})
+        // Create refresh token
+        const refresh = UserController.genRefreshToken({_id: newUser._id});
 
-            res.status(201).json({
+        // Store in httpOnly cookie
+        res.cookie("refresh-token", refresh, 
+        {
+            httpOnly: true,
+            sameSite: "None",
+            secure: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+
+        // Respond with token and user info
+        res.status(201).json({
+            user: {
                 _id: newUser._id,
                 username: newUser.username,
                 email: newUser.email,
-                token: UserController.genJWT(newUser._id),
-            });
-        }
-        catch(e){
-            console.error(e);
-            next(e);
-        }
-
-    },
+            },
+            token: UserController.genJWT(newUser._id),
+        });
+        
+    }),
 
     /*
-    @desc:    
-    @route:   
-    @access:  
+    @desc:    Verifies a user's credentials, sends back user info and auth token
+    @route:   POST /api/user/authenticate
+    @access:  Public
     */
-    authenticateUser: async (req, res, next) => {
-        try {
-            const credentials = {
-                email: req.body.email,
-                password: req.body.password,
-            };
+    authenticateUser: AsyncHandler( async (req, res) => {
+        const credentials = {
+            email: req.body.email,
+            password: req.body.password,
+        };
 
-            if(!credentials.email) return res.status(400).json({msg: "Email field required"});
+        if(!credentials.email) return res.status(400).json({msg: "Email field required"});
 
-            if(!credentials.password) return res.status(400).json({msg: "Password required"});
+        if(!credentials.password) return res.status(400).json({msg: "Password required"});
 
-            const user = await User.findOne({email: credentials.email});
+        const user = await User.findOne({email: credentials.email});
 
-            if(!user) return res.status(400).json({"msg": "A user with that email does not exist"});
+        if(!user) return res.status(400).json({"msg": "A user with that email does not exist"});
 
-            const match = await bcrypt.compare(credentials.password, user.password);
+        const match = await bcrypt.compare(credentials.password, user.password);
 
-            if(!match) return res.status(400).json({msg: "Incorrect password"});
+        if(!match) return res.status(400).json({msg: "Incorrect password"});
 
-            res.status(200).json({
-                _id: user._id,
+        // Create refresh token
+        const refresh = UserController.genRefreshToken({_id: user._id});
+
+        // Store in httpOnly cookie
+        res.cookie("refreshToken", refresh, 
+        {
+            httpOnly: true,
+            sameSite: "None",
+            secure: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+
+        res.status(200).json({
+            user: {
                 username: user.username,
                 email: user.email,
-                token: UserController.genJWT(user._id),
-            })
+            },
+            token: UserController.genJWT(user._id),
+        })
+    }),
+
+    refreshAccess: AsyncHandler( async (req, res) => {
+        // Check if there is a token in cookies
+        if(!req.cookies?.refreshToken) {
+            return res.status(401).json([{msg: "Not authorized, no refresh token"}]);
         }
-        catch(e) {
-            console.error(e);
-            next(e);
-        }
+
+        // Get token from cookies
+        const refreshToken = req.cookies.refreshToken;
+
+        // Verify token
+        jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, 
+            async (err, decoded) => {
+                // Token invalid
+                if(err) return res.status(401).json([{msg: "Not authorized"}]);
+
+                // Token valid, make a new access token and respond
+                const accessToken = UserController.genAuthToken(req.user._id);
+                return res.json({token: accessToken});
+            }
+        );
+    }),
+
+    logoutUser: (req, res, next) => {
+        res.clearCookie("refreshToken");
+        res.status(200).json([{msg: "Success"}]);
     },
 
     /*
@@ -99,14 +149,23 @@ const UserController = {
     },
 
     /*
-    @desc:     
-    @params:   
-    @returns:  
+    @desc:     Generates a json web token for authorization
+    @params:   id: id of the user attempting to generate token
+    @returns:  json web token with id stored
     */
-    genJWT: (id) => {
-        return jwt.sign({id}, process.env.JWT_SECRET, {
-            expiresIn: "30d",
+    genAuthToken: (id) => {
+        return jwt.sign({id}, process.env.JWT_AUTH_SECRET, {
+            expiresIn: "15m",
         });
+    },
+
+    /*
+    @desc:     Generates a json web token for generating auth tokens
+    @params:   id: id of the user attempting to generate token
+    @returns:  json web token with id stored
+    */
+    genRefreshToken: (payload) => {
+        return jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: "7d"});
     },
 
 };
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,30 +5,51 @@ const User = require("../models/User");
 
 module.exports = {
 
-    authProtect: async (req, res, next) => {
-        let token;
+    // authProtect: async (req, res, next) => {
+    //     let token;
 
-        if( req.headers.authorization && req.headers.authorization.startsWith("Bearer") ) {
-            try {
-                token = req.headers.authorization.split(" ")[1]; // Token comes after "Bearer (ie: Bearer tokenDataHere")
+    //     if( req.headers.authorization && req.headers.authorization.startsWith("Bearer") ) {
+    //         try {
+    //             token = req.headers.authorization.split(" ")[1]; // Token comes after "Bearer (ie: Bearer tokenDataHere")
 
-                const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    //             const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-                req.user = await User.findById(decoded.id).select("-password");
+    //             req.user = await User.findById(decoded.id).select("-password");
 
+    //             next();
+    //         }
+    //         catch(e) {
+    //             console.error(e);
+    //             res.status(401);
+    //             throw new Error("Not authorized");
+    //         }
+    //     }
+
+    //     if(!token) {
+    //         res.status(401);
+    //         throw new Error("Not authorized, no token");
+    //     }
+    // },
+
+    protectRoute: async (req, res, next) => {
+        // Check if there is a token
+        const token = (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) ? req.headers.authorization.split(" ")[1] : null;
+
+        // If there was not a token, respond
+        if(!token) return res.status(401).json([{msg: "Not authorized, no token"}]);
+
+        // Decode token
+        jwt.verify(token, preovess.env.JWT_AUTH_SECRET, 
+            async (err, decoded) => {
+                // If token is invalid, respond
+                if(err) return res.status(401).json([{msg: "Not authorized"}]);
+
+                // Set user
+                req.user = await User.findById(decoded._id);
+
+                // Do next thingy
                 next();
-            }
-            catch(e) {
-                console.error(e);
-                res.status(401);
-                throw new Error("Not authorized");
-            }
-        }
-
-        if(!token) {
-            res.status(401);
-            throw new Error("Not authorized, no token");
-        }
+        });
     },
 
 };
