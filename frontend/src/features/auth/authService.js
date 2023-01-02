2cb7889d33dbf1774ccc001d7ec310ee795a6786
Tyler Perata <tylerperata@gmail.com>
2023-01-02 15:35:20 -0800

    Frontend changes

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -13,6 +13,23 @@ const register = async (userData) => {
     return res.data;
 }
 
+// Log user in
+const login = async (userData) => {
+    const res = await axios.post(API_URL + "authenticate", userData);
+
+    if(res.data) {
+        localStorage.setItem("user", JSON.stringify(res.data));
+    }
+
+    return res.data;
+}
+
+const logout = async () => {
+    const res = await axios.delete(API_URL + "logout");
+
+    localStorage.removeItem("user");
+}
+
 const authService = {
     register,
 }
