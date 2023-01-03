333601f1f0aa5fafc7b3e542e47e0b8f00f7270f
Tyler Perata <tylerperata@gmail.com>
2023-01-02 16:25:04 -0800

    Add basic api index html page and 404 page

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-
-// Use .env file for environement variables
 require("dotenv").config(); 
+const express = require("express");
+const app = express();
+const path = require("path");
 
 const port = process.env.PORT || 3000;
 
@@ -11,21 +11,31 @@ const {errorHandler} = require("./middleware/errorHandler");
 // Connect to MongoDB via Mongoose
 (require("./config/db"))();
 
-const app = express();
-
-
 const cors = require("cors");
 app.use(cors({credentials: true, origin: "http://localhost:5000"}));
-
-const cookieParser = require("cookie-parser");
-app.use(cookieParser());
 
 // Body middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
 // Router
 app.use("/", require("./routes/index.js"));
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
 
 // Use error handler
 app.use(errorHandler);
