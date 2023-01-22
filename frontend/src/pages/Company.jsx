1d1ab4f5866b13bdabdfe1528f32cea69ca6248f
Tyler Perata <tylerperata@gmail.com>
2023-01-22 14:34:15 -0800

    Style changes

--- frontend/src/pages/Company.jsx
+++ frontend/src/pages/Company.jsx
@@ -14,17 +14,34 @@ function Company({companies}) {
 
 
   return (
-    <section>
-        <h1>{company.name}</h1>
-        <section>
-          <h2>Positions</h2>
-          <div>
+    <section
+      className="w-[95%] mx-auto"
+    >
+        <section
+          className="mb-6"
+        >
+          <h1
+            className="text-3xl text-blue border-b-2 border-blue mb-2"
+          >{company.name}</h1>
+          <p>A large scale video sharing company, owned by Google</p>
+        </section>
+        <section
+          className="mb-6"
+        >
+          <h2
+            className="text-2xl mb-2 border-b border-blue"
+          >Positions</h2>
+          <div 
+            className="max-h-[600px] overflow-y-scroll"
+          >
             {
               company.positions.map(x => <Position id={x.name} position={x} />)
             }
           </div>
         </section>
-        <section>
+        <section
+          className="mb-6"
+        >
           <h2>Connections</h2>
           <div>
             {
@@ -32,7 +49,6 @@ function Company({companies}) {
             }
           </div>
         </section>
-        <p>Descriptive info</p>
     </section>
   )
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,7 +5,7 @@ function Dashboard({companies}) {
   const taskCardStyle = "border-4 rounded-md px-4 py-2 mb-2";
 
   return (
-    <section className="max-w-7xl mx-auto">
+    <section className="">
       
       <h1 className="text-3xl ml-10 mb-6 flex items-center">
         <MdWavingHand className="mr-4" />
