82d4c69224c27ffc2520de8f9f8091680a879fa5
Tyler Perata <tylerperata@gmail.com>
2023-01-25 23:01:52 -0800

    Create and integrate TaskList to Dashboard, clean Dashboard of unused code

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,16 +1,9 @@
 import {MdWavingHand} from "react-icons/md";
-import {FaPlus} from "react-icons/fa";
-import {Link} from "react-router-dom";
 
-import TaskCard from "../components/TaskCard";
-import OptionButton from "../components/OptionButton";
-import CreateCompanyForm from "../components/forms/CreateCompanyForm";
-import OptionHeader from "../components/OptionHeader";
 import CompanyList from "../components/lists/CompanyList";
+import TaskList from "../components/lists/TaskList";
 
 function Dashboard({companies}) {
-
-  const taskCardStyle = "border-4 rounded-md px-4 py-2 mb-2";
 
   const tasks = [
     {
@@ -64,50 +57,12 @@ function Dashboard({companies}) {
       </h1>
 
       <section className="w-[95%] flex flex-col mx-auto">
-        <section
-        className="w-full h-[300px] mb-10 py-2"
-        >
-          <OptionHeader text="Upcoming Tasks"/>
-          <section className="overflow-scroll h-[90%] px-2">
-            {
-            (tasks.length) ? 
-            tasks.map((x,i) => <TaskCard task={x} id={i} />)
-            :
-            <p
-              className="mx-auto text-gray-500 text-xl"
-            >No upcoming tasks</p>
-            }
-          </section>
-        </section>
+        <TaskList tasks={tasks} />
 
         <CompanyList companies={companies} />
 
       </section>
 
-    </section>
-  )
-}
-
-function CompanyShort({company}) {
-  const {name, description} = company;
-  let positionCount = company.positions.length-1;
-
-  return(
-    <section className="w-full h-40 mb-5 shadow-xl p-5 bg-white">
-      <h2 className="text-lg mb-3">
-        <Link
-          to={`/companies/${company._id}`}
-          className="cursor-pointer"
-        >
-          <span className="text-orange">{name} </span> 
-          &ndash;
-          <span> {company.positions[0].name}</span>
-          { (positionCount-1 > 0) && <span>+{positionCount}</span> }
-        </Link>
-      </h2>
-      <p className="h-[60px] p-1 overflow-scroll">
-        {description}
-      </p>
     </section>
   )
 }
