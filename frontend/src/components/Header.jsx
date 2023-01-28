ce4556eba050b07a23085456c3a8bd0fdd320356
Tyler Perata <tylerperata@gmail.com>
2023-01-27 22:50:33 -0800

    Header style change

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -12,7 +12,7 @@ const logoStyles = " text-darkBlue font-bold text-2xl";
 
 const ulStyles = "flex justify-evenly space-x-10";
 
-const linkStyles = "flex items-center space-x-2 p-1 hover:text-lightBlue";
+const linkStyles = "w-fit flex items-center space-x-2 p-1 hover:text-lightBlue";
 
 function Header() {
 
@@ -58,7 +58,7 @@ function Header() {
           </div>
 
           <div className="md:hidden">
-              <div id="menu" className={"absolute flex-col items-center self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md" + (navOpen ? "" : " hidden") }>
+              <div id="menu" className={"absolute flex flex-col items-center self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md" + (navOpen ? "" : " hidden") }>
                 {signedIn ? 
                   (
                     <Link className={linkStyles} to="/logout">
