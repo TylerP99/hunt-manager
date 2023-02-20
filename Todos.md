7946ea6db65a78636efcd055331ccde2168d93d5
Tyler Perata <tylerperata@gmail.com>
2023-02-19 20:48:09 -0800

    Change

---
+++ Todos.md
@@ -0,0 +1,6 @@
+# TODO
+
+- [ ] Add information to landing page
+- [ ] Add information to footer
+- [ ] Create basic task system with mark complete function, where tasks are automatically created by company
+- [ ] 
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,6 +1,11 @@
 function Landing() {
   return (
-    <div>Landing</div>
+    <div>
+      <main>
+        <h1>Hunt Manager</h1>
+        <p>Manage job applications the smart way</p>
+      </main>
+    </div>
   )
 }
 
