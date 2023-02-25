9b63d7e4c6937199ba92de777ceee8a1b4031d39
Tyler Perata <tylerperata@gmail.com>
2023-02-25 10:25:50 -0800

    Landing changes

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -5,6 +5,9 @@ function Landing() {
         <h1>Hunt Manager</h1>
         <p>Manage job applications the smart way</p>
       </main>
+      <section>
+        <p>Hunt Manager is a web application for tracking different job opportunities. Hunt Manager encourages a networking focused approach to finding a job. Spefically, this application was created using fundamentals tought within the 100Devs training program for finding a job in tech. Here, users can add job opportunities to a list, track contacts within companies, draft messages, and ultimately find their next job. Get started now!</p>
+      </section>
     </div>
   )
 }
